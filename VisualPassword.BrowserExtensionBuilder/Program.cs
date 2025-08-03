b4e8e39454b04331edb21593b01e81441ba428ef
viruseg <virus85@yandex.ru>
2025-08-03 17:38:22 +0300

    BrowserExtensionBuilder. The first version.

---
+++ VisualPassword.BrowserExtensionBuilder/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Compression;
+using System.Text.RegularExpressions;
+using Utils;
+
+Build("VisualPassword.Chrome");
+Build("VisualPassword.Firefox");
+return;
+
+void Build(string name)
+{
+    var sourceManifestPath = $"{AppContext.BaseDirectory}{name}{Path.DirectorySeparatorChar}manifest.json";
+    var sourceHtmlPath = $"{AppContext.BaseDirectory}VisualPassword.Web{Path.DirectorySeparatorChar}index.html";
+    var sourceIconPath = $"{AppContext.BaseDirectory}VisualPassword.Web{Path.DirectorySeparatorChar}ico.png";
+
+    var targetBuildPath = $"{AppContext.BaseDirectory}Build{Path.DirectorySeparatorChar}";
+    var targetExtPath = $"{targetBuildPath}{name}.Extension{Path.DirectorySeparatorChar}";
+    var targetHtmlPath = $"{targetExtPath}index.html";
+    var targetIconPath = $"{targetExtPath}ico48.png";
+    var targetJsPath = $"{targetExtPath}script.js";
+    var targetManifestPath = $"{targetExtPath}manifest.json";
+    var targetZipPath = $"{targetBuildPath}{name}.Extension.zip";
+
+    if (Directory.Exists(targetExtPath)) Directory.Delete(targetExtPath, true);
+    Directory.CreateDirectory(targetExtPath);
+
+    var html = File.ReadAllText(sourceHtmlPath);
+    var js = "";
+    var manifest = File.Exists(sourceManifestPath) ? File.ReadAllText(sourceManifestPath) : "";
+    var icon = File.Exists(sourceIconPath) ? File.ReadAllBytes(sourceIconPath) : null;
+
+    var match = ScriptRegex().Match(html);
+    if (match.Success) js = match.Groups[1].Value;
+
+    html = ScriptRegex().Replace(html, """<script src="script.js"></script>""");
+    html = html.Replace("<body>", """<body class="chromeExtension">""");
+
+    File.WriteAllText(targetHtmlPath, html);
+    File.WriteAllText(targetJsPath, js);
+    File.WriteAllText(targetManifestPath, manifest);
+    if (icon != null) File.WriteAllBytes(targetIconPath, icon);
+
+    var zipFolder = new ZipFolder(targetZipPath);
+    zipFolder.WriteAllText("index.html", html, CompressionLevel.SmallestSize);
+    zipFolder.WriteAllText("script.js", js, CompressionLevel.SmallestSize);
+    zipFolder.WriteAllText("manifest.json", manifest, CompressionLevel.SmallestSize);
+    if (icon != null) zipFolder.WriteAllBytes("ico48.png", icon, CompressionLevel.SmallestSize);
+}
+
+internal abstract partial class Program
+{
+    [GeneratedRegex(@"(?s)<script\b[^>]*>(.*?)</script>")]
+    private static partial Regex ScriptRegex();
+}
---
+++ VisualPassword.BrowserExtensionBuilder/ZipFolder.cs
@@ -0,0 +1,86 @@
+﻿using System.IO.Compression;
+
+namespace Utils;
+
+public sealed class ZipFolder
+{
+    private readonly string zipPath;
+
+    public ZipFolder(string zipPath)
+    {
+        this.zipPath = zipPath;
+
+        if (!File.Exists(zipPath)) CreateEmptyArchive();
+    }
+
+    private void CreateEmptyArchive()
+    {
+        using var archive = ZipFile.Open(zipPath, ZipArchiveMode.Create);
+    }
+
+    public string ReadAllText(string entryName)
+    {
+        var bytes = ReadAllBytes(entryName);
+        return System.Text.Encoding.UTF8.GetString(bytes);
+    }
+
+    public byte[] ReadAllBytes(string entryName)
+    {
+        using var archive = ZipFile.Open(zipPath, ZipArchiveMode.Read);
+        var entry = archive.GetEntry(entryName);
+
+        if (entry == null) throw new FileNotFoundException($"The file '{entryName}' was not found in the archive.");
+
+        using var memoryStream = new MemoryStream();
+        using (var entryStream = entry.Open())
+        {
+            entryStream.CopyTo(memoryStream);
+        }
+
+        return memoryStream.ToArray();
+    }
+
+    public void WriteAllText(string entryName, string content, CompressionLevel compressionLevel = CompressionLevel.NoCompression)
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(content);
+        WriteAllBytes(entryName, bytes, compressionLevel);
+    }
+
+    public void WriteAllBytes(string entryName, byte[] content, CompressionLevel compressionLevel = CompressionLevel.NoCompression)
+    {
+        using var archive = ZipFile.Open(zipPath, ZipArchiveMode.Update);
+
+        var existingEntry = archive.GetEntry(entryName);
+        existingEntry?.Delete();
+
+        var newEntry = archive.CreateEntry(entryName, compressionLevel);
+
+        using var entryStream = newEntry.Open();
+        entryStream.Write(content, 0, content.Length);
+    }
+
+    public bool FileExists(string entryName)
+    {
+        using var archive = ZipFile.OpenRead(zipPath);
+        return archive.GetEntry(entryName) != null;
+    }
+
+    public string[] GetEntries(string? path)
+    {
+        if (path == null) path = "/";
+        path = path.Replace('\\', '/');
+        if (!path.EndsWith('/')) path += '/';
+        path = path.TrimStart('/');
+
+        var countSlashes = path.Count(c => c == '/');
+
+        using var archive = ZipFile.OpenRead(zipPath);
+
+        return archive.Entries
+                      .Select(p => p.FullName)
+                      .Where(p => p.StartsWith(path, StringComparison.OrdinalIgnoreCase) &&
+                                  (p.EndsWith('/') && p.Count(c => c == '/') == countSlashes + 1 ||
+                                   p != path && p.Count(c => c == '/') == countSlashes))
+                      .ToArray();
+    }
+}
